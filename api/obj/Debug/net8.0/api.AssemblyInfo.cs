4dab430282e864824a3a8c29733895d8940be3f2
Fatihk239 <fatihk239@gmail.com>
2024-08-22 10:58:47 +0300

    async

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e83d48707cd9076545da61919133de8f221c1b83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+643c9e31861a2039b4e25a18e9c53810cf8640e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
