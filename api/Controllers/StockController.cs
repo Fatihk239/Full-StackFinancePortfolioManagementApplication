c39fa0ca860f5aa25cbcd9e8cfa1a8de39fda7f5
Fatihk239 <fatihk239@gmail.com>
2024-08-27 03:37:09 +0300

    26_PortfolioGET

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -8,6 +8,7 @@ using api.Helpers;
 using api.Interfaces;
 using api.Mappers;
 using api.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Client;
@@ -28,6 +29,7 @@ namespace api.Controllers
             _context = context;
         }     
             [HttpGet]
+            [Authorize]
             public async  Task<IActionResult> GetAll([FromQuery] QueryObject query)
             {   
                 if(!ModelState.IsValid)
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1e1203a0f941a142145be357e059d05d62f6b9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0b0f0ebf9d78314c79bb58d93c5b6440f2b23dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
